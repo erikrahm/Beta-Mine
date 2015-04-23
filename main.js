16f49c71e3089a4d9943054dff3bfeb880244b74
Erik <erik@Deborahs-iMac.local>
2015-04-23 16:17:05 -0500

    Test

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ var express = require('express')
 var app = express();
 var http = require ('http');
 var mongoose = require ("mongoose");
+var bodyParser = require ("body-parser");
 
 // Disable etag headers on responses
 app.disable('etag');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // set the static files location /public/img will be /img for users
 app.use(express.static(__dirname + '/public')); 
@@ -13,7 +19,7 @@ app.use(express.static(__dirname + '/public'));
 var uristring =
 process.env.MONGOLAB_URI ||
 process.env.MONGOHQ_URL ||
-' mongodb://<dbuser>:<dbpassword>@ds037617.mongolab.com:37617/heroku_app36147927';
+'mongodb://heroku_app36147927:<?php3choAbicat817?>@ds037617.mongolab.com:37617/heroku_app36147927';
 
 var theport = process.env.PORT || 5000;
 
@@ -21,7 +27,7 @@ var theport = process.env.PORT || 5000;
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err.message);
   } else {
   console.log ('Succeeded connected to: ' + uristring);
   }
@@ -29,15 +35,115 @@ mongoose.connect(uristring, function (err, res) {
 //MONGOOSE CONNECTION END
 
 //EXPRESS ROUTES START
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
 
+/*
 //ROUTES
 require('./app/routes')(app); // configure our routes
 
+*/
+
+//EXPRESS ROUTES END
+
+var Bear = require('./app/models/bear');
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+// create our router
+var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+	// do logging
+	console.log('Something is happening.');
+	next();
+});
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+	// create a bear (accessed at POST http://localhost:8080/bears)
+	.post(function(req, res) {
+		
+		var bear = new Bear();		// create a new instance of the Bear model
+		bear.name = req.body.name;  // set the bears name (comes from the request)
+
+		bear.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Bear created!' });
+		});
+
+		
+	})
+
+	// get all the bears (accessed at GET http://localhost:8080/api/bears)
+	.get(function(req, res) {
+		Bear.find(function(err, bears) {
+			if (err)
+				res.send(err);
+
+			res.json(bears);
+		});
+	});
+
+// on routes that end in /bears/:bear_id
+// ----------------------------------------------------
+router.route('/bears/:bear_id')
+
+	// get the bear with that id
+	.get(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+			if (err)
+				res.send(err);
+			res.json(bear);
+		});
+	})
+
+	// update the bear with this id
+	.put(function(req, res) {
+		Bear.findById(req.params.bear_id, function(err, bear) {
+
+			if (err)
+				res.send(err);
+
+			bear.name = req.body.name;
+			bear.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Bear updated!' });
+			});
+
+		});
+	})
+
+	// delete the bear with this id
+	.delete(function(req, res) {
+		Bear.remove({
+			_id: req.params.bear_id
+		}, function(err, bear) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/api', router);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
-//EXPRESS ROUTES END
+
 
